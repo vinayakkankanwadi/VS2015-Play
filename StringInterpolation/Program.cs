11c876637d338e052a1b57095c6b1c871908ca72
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 15:53:07 +1000

    StringInterpolation

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace StringInterpolation
+{
+    class StringInterpolations
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        static void Main(string[] args)
+        {
+            StringInterpolations si = new StringInterpolations();
+            si.FirstName = "Visual";
+            si.LastName = "Studio";
+            si.Age = 2015;
+
+            WriteLine(string.Format("Old Way Name : {0} {1}\nAge : {2}", si.FirstName, si.LastName, si.Age));     // Old Way      
+            // Putting String Where I need 
+            WriteLine($"New Way Name : {si.FirstName} {si.LastName}\nAge : {si.Age}");            // New Way
+            ReadLine();
+        }
+    }
+}
