96e35b6543cd9d106adc49eab0e87713f6ae4697
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 15:38:27 +1000

    NameOfExpressions

---
+++ NameOfExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace NameOfExpressions
+{
+    class Program
+    {
+        static void DisplayName(string name)
+        {
+            try
+            {
+                //throw new Exception("name");          // Before
+                throw new Exception(nameof(name));      // name of resolves issues with hard coded string literals when passing them as parameter          
+            }
+            catch (Exception ex) when (ex.Message == nameof(name))
+            {
+                WriteLine("Expection for "+ex.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayName("Dev");
+        }
+    }
+}
