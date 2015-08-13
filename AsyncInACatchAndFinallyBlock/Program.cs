01a972b92232d7310555ecad393a6096d8a4df04
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 15:07:12 +1000

    Async in a Catch and Finally Block

---
+++ AsyncInACatchAndFinallyBlock/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace AsyncInACatchAndFinallyBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Before caller " + Thread.CurrentThread.ManagedThreadId);
+                CallerMethod();
+                Console.WriteLine("After caller " + Thread.CurrentThread.ManagedThreadId);
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+        }
+
+        public static async void CallerMethod()
+        {
+            try
+            {
+                throw new Exception("Error");
+            }
+            catch (Exception)
+            {
+                await Task.Delay(2000);
+                WriteLine("Waiting 2 seconds in catch");
+            }
+            finally
+            {
+                await Task.Delay(2000);
+                WriteLine("Waiting 2 seconds in finally");
+            }
+        }        
+    }
+}
