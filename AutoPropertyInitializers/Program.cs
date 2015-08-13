102f7a20625416d68dee178017e66a0c5fff5b06
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 14:23:10 +1000

    Static Using Syntax and AutoPropertyInitializers

---
+++ AutoPropertyInitializers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AutoPropertyInitializers
+{
+    /*public class Customer
+    {
+        public Customer()
+        {
+            customerID = Guid.NewGuid();
+        }
+
+        public Guid customerID { get; set; }
+    }*/
+
+    public class Customer
+    {
+        public Guid customerID { get; set; } = Guid.NewGuid();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StaticUsingSyntax/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console; //Note static keyword
+
+namespace StaticUsingSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
