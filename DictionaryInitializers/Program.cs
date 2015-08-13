ff79771f4a7e785248c616587f92bd3653e9ca3a
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 14:32:20 +1000

    DictionaryInitializers

---
+++ DictionaryInitializers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+
+namespace DictionaryInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dictionary<string, string> customerNames = new Dictionary<string, string>()
+            {
+                { "Michael Jordan", "Basketball" },
+                { "Peyton Manning", "Football" },
+                { "Babe Ruth", "Baseball" }
+            };*/
+
+            Dictionary<string, string> customerNames = new Dictionary<string, string>()
+            {
+                ["Michael Jordan"] = "Basketball",
+                ["Peyton Manning"] = "Football",
+                ["Babe Ruth"] = "Baseball"
+            };
+        }
+    }
+}
