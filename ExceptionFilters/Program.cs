b8eb4b3a5f9b3807780037fff6d03cc9c47ef997
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 14:40:22 +1000

    ExceptionFilters

---
+++ ExceptionFilters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace ExceptionFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("Error");
+            }
+            catch (Exception ex) when (ex.Message == "Error1")
+            {
+                WriteLine("Caught Error1");
+            }
+            catch (Exception ex) when (ex.Message == "Error")
+            {
+                WriteLine("Caught Error");
+            }
+        }
+    }
+}
