6df9c12b8754b990a51098ed35a5e642f7008de8
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2015-08-13 16:13:57 +1000

    ConditionalAccessOperator

---
+++ ConditionalAccessOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace ConditionalAccessOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayName("Test");
+            DisplayName(null);
+        }
+
+        public static void DisplayName(String name)
+        {
+            /*string dispName = "Guest";    // Old Way 
+            if( name != null)
+            {
+                dispName = name;
+            }*/
+
+            string dispName = name?.ToString() ?? "Guest"; // New way using ?. to check null
+            WriteLine($"Hello {dispName}");
+        }
+    }
+}
